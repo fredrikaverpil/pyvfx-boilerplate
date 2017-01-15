d01873c24a54fed9d422b831721ac67529564333
Fredrik Averpil <fredrik.averpil@gmail.com>
2017-01-15 22:24:42 +0100

    General cleanup

--- boilerplate.py
+++ boilerplate.py
@@ -53,21 +53,11 @@ DOCK_WITH_NUKE_UI = False
 REPO_PATH = '/Users/fredrik/code/repos/pyvfx-boilerplate/'
 
 # Palette filepath
-PALETTE_FILEPATH = os.path.join(REPO_PATH, 'boilerdata',
-                                           'qpalette_maya2016.json')
+PALETTE_FILEPATH = os.path.join(
+    REPO_PATH, 'boilerdata', 'qpalette_maya2016.json')
 
 # Full path to where .ui files are stored
 UI_PATH = os.path.join(REPO_PATH, 'boilerdata')
-
-# Find Qt.py module and setup site-packages accordingly
-QTPY_SITE_SEARCH_PATHS = [
-    'C:/Python27/Lib/site-packages',  # Windows
-    '/usr/lib/python2.7/site-packages',  # Linux
-    '/Library/Python/2.7/site-packages',  # OS X
-
-    '/usr/local/Cellar/python/2.7.11/Frameworks/Python.framework/Versions/' +
-    'Current/lib/python2.7/site-packages'  # Homebrew location, Python 2.7.11
-]
 
 # Qt.py option: Set up preffered binding
 # os.environ['QT_PREFERRED_BINDING'] = 'PyQt4'
@@ -80,52 +70,8 @@ if NUKE:
 
 
 # ----------------------------------------------------------------------
-# Python site packages helper functions
-# ----------------------------------------------------------------------
-
-def _find_qtpy(search_paths, register=False):
-    """Searches for Qt.py and registers the site path"""
-    for search_path in search_paths:
-        if os.path.exists(search_path):
-            for item in os.listdir(search_path):
-                if item == 'Qt.py':
-                    if register and search_path not in sys.path:
-                        site.addsitedir(search_path)  # Add site path
-                        print('Added site-packages from ' + search_path)
-                    return True
-
-
-def _sitepackages_setup(additional_search_paths):
-    # Make non-standalone DCC application find Qt.py module
-    site_paths = site.getsitepackages()
-    if not _find_qtpy(site_paths):
-        if not _find_qtpy(additional_search_paths, register=True):
-            raise ImportError('Could not find Qt.py module.')
-
-
-def _ui_dir(folderpath):
-    """Attempt to auto-detect path to ui files"""
-
-    if os.path.exists(folderpath):
-        return folderpath
-    else:
-        try:
-            boilerplate_dir = os.path.dirname(__file__)
-            ui_dir = os.path.join(boilerplate_dir, 'data')
-            return ui_dir
-        except NameError:
-            raise IOError('Could not locate .ui directory.')
-
-
-# ----------------------------------------------------------------------
 # Set up Python modules access
 # ----------------------------------------------------------------------
-
-# Set up filepath in order to find .ui files (required for Maya and Nuke)
-ui_dir = _ui_dir(UI_PATH)
-
-# Find Qt.py module and setup site-packages accordingly
-_sitepackages_setup(QTPY_SITE_SEARCH_PATHS)
 
 # Enable access to boilerlib
 if REPO_PATH not in sys.path:
@@ -171,8 +117,8 @@ class Boilerplate(QtWidgets.QMainWindow):
             self.setProperty("saveWindowPref", True)
 
         # Filepaths
-        main_window_file = os.path.join(ui_dir, 'main_window.ui')
-        module_file = os.path.join(ui_dir, 'module.ui')
+        main_window_file = os.path.join(UI_PATH, 'main_window.ui')
+        module_file = os.path.join(UI_PATH, 'module.ui')
 
         # Load UIs
         self.main_widget = load_ui(main_window_file)  # Main window UI
@@ -284,9 +230,9 @@ def run_maya():
     if not DOCK_WITH_MAYA_UI:
         boil.show()  # Show the UI
     elif DOCK_WITH_MAYA_UI:
-        allowedAreas = ['right', 'left']
+        allowed_areas = ['right', 'left']
         cmds.dockControl(WINDOW_TITLE, label=WINDOW_TITLE, area='left',
-                         content=WINDOW_OBJECT, allowedArea=allowedAreas)
+                         content=WINDOW_OBJECT, allowedArea=allowed_areas)
 
 
 def run_nuke():
